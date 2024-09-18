882281abbf4aeadbadee9d885da6039531ea8f59
Daniel Kim <daniel.kim.tor15@gmail.com>
2024-09-18 04:16:18 -0400

    organize code into function, added comments and updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # grafana-alert-copier
+
+This is a tool designed to copy alerting configurations from one Grafana instance to another. It uses the Grafana API to fetch and import contact points, notification policies and alerts.
+
+## How it works
+
+1. The tool first copies over the contact points from the source Grafana instance.
+
+    - It fetches the contact points from the source Grafana instance and filters out the default created contact point, "email receiver".
+
+    - For each contact point in the source Grafana instance, it exports the contact point (excluding the UID) and imports it to the target Grafana instance.
+
+    - If the contact point already exists in the target Grafana instance, it importing the new one before deleting the existing contact point.
+
+2. The tool then copies the notification policies from the source Grafana instance.
+
+    - It fetches the notification policy tree from the source Grafana instance.
+
+    - It pushs the fetched notification policy tree to the target Grafana instance. 
+    
+    - It completely replaces the notification policy tree in the target instance(No notification policy in the target instance is saved).
+
+3. The tool then copies the folders from the source Grafana instance.
+
+    - It fetches all folders from the source Grafana instance.
+
+    - For each folder, it exports the folder (excluding the UID) and imports it to the target Grafana instance.
+
+    - If the folder already exists in the target Grafana instance, it will deny the creation.    
+
+4. It also fetches all alerts from the source Grafana instance. 
+
+    * It fetches the export for all alerts in the source Grafana instance. For each alert export:
+   
+        * It finds the title of the folder that the alert belongs to in the source Grafana instance.
+
+        * It finds the UID of the folder with the same title in the target Grafana instance.
+
+        * It updates the alert export's `folderUID` to the UID of the folder in the target Grafana instance and removes the `uid` and `updated` fields from the alert export.
+
+        * It checks if an alert with the same title and `folderUID` exists in the target Grafana instance. If it does, it deletes that alert.
+
+        * Finally, it imports the updated alert export into the target Grafana instance.
+
+
+## Prerequisites
+
+- jq: a lightweight and flexible command-line JSON processor.
+- curl: a command-line tool for getting or sending data including files using URL syntax.
+- xargs: a command-line utility for building and executing command lines from standard input.
+
+## Command Line Arguments
+
+The script accepts the following command line arguments:
+
+- `-S` or `--source-bearer`: The bearer token for the source Grafana instance.
+- `-s` or `--source-address`: The address (hostname or IP) of the source Grafana instance.
+- `-T` or `--target-bearer`: The bearer token for the target Grafana instance.
+- `-t` or `--target-address`: The address (hostname or IP) of the target Grafana instance.
+
+You can pass these arguments to the script in the following way:
+
+```shell
+./alertCopy.sh -S <source_bearer> -s <source_address> -T <target_bearer> -t <target_address>
+```
+
+## Example
+
+```shell
+./alertCopy.sh -S glsa_exampleGlMvU7 -s vfl-069.engage.sas.com -T glsa_exampleGlMvU7 -t vfl-069.engage.sas.com
+```
+
+## Note
+
+This tool assumes that you have the necessary permissions to fetch and update the Grafana configurations. Also, it doesn't handle any errors that might occur during the process. You might want to add error handling based on your requirements.
--- alertCopy.sh
+++ alertCopy.sh
@@ -46,129 +46,184 @@ if [ "$t_provided" -eq 0 ] || [ "$T_provided" -eq 0 ] || [ "$s_provided" -eq 0 ]
     usage
 fi
 
-# Export contact points from source Grafana instance
-source_contact_points="$(curl -s \
-    -H "Authorization: Bearer $source_bearer" \
-    "https://$source_address/grafana/api/v1/provisioning/contact-points"
-)"
+#Function to copy contact points from source instance to target instance. Adds contact points that exist in the source instance but not in the target instance. Keeps contact points that only exists in the target instance. If a contact point with the same name already exists in the target instance, it will be replaced by the source instance contact point.
+copy_contact_points() {
+    # Define local variables for source and target Grafana instances
+    local source_bearer=$1
+    local source_address=$2
+    local target_bearer=$3
+    local target_address=$4
 
-# Contact points in the target Grafana
-target_contact_points="$(curl -s \
-    -H "Authorization: Bearer $target_bearer" \
-    "https://$target_address/grafana/api/v1/provisioning/contact-points"
-)"
+    # Fetch contact points from source Grafana instance
+    source_contact_points="$(curl -s \
+        -H "Authorization: Bearer $source_bearer" \
+        "https://$source_address/grafana/api/v1/provisioning/contact-points"
+    )"
 
-declare -A target_contact_name_to_uid
-
-filtered_contact_points=$(echo "$target_contact_points" | jq -r -c 'del(.[] | select(.name == "email receiver")) | .[]')
-
-if [ -n "$filtered_contact_points" ]; then
-    while read -r target_contact_point;
-    do 
-        name="$(echo "$target_contact_point" | jq -r '.name' | xargs)"
-        uid="$(echo "$target_contact_point" | jq -r '.uid' | xargs)"
-        # echo $name
-        # echo $uid
-        target_contact_name_to_uid[$name]=$uid
-    done <<< "$filtered_contact_points"
-fi
-
-
-echo "$source_contact_points" | jq -r -c 'del(.[] | select(.name == "email receiver")) | .[]' | while read -r source_contact_point;
-do  
-    source_contact_point_name=$(echo "$source_contact_point" | jq -r '.name' | xargs)
-    source_contact_point_export="$(echo $source_contact_point | jq -r 'del(.uid)')"
-    
-    # Import contact points
-    curl -s \
-        -d "$source_contact_point_export" \
-        -H 'X-Disable-Provenance: true' \
-        -H 'Content-Type: application/json' \
-        -H 'Accept: application/json' \
+    # Fetch contact points from target Grafana instance
+    target_contact_points="$(curl -s \
         -H "Authorization: Bearer $target_bearer" \
         "https://$target_address/grafana/api/v1/provisioning/contact-points"
-    
-    if [[ -v "target_contact_name_to_uid[$source_contact_point_name]" ]]; then
-        curl -s \
-            --request "DELETE" \
-            -H "Authorization: Bearer $target_bearer" \
-            "https://$target_address/grafana/api/v1/provisioning/contact-points/${target_contact_name_to_uid[$source_contact_point_name]}"
+    )"
+
+    # Declare an associative array to map contact point names to their UIDs in the target instance
+    declare -A target_contact_name_to_uid
+
+    # Filter out the "email receiver" contact point from the target contact points
+    filtered_contact_points=$(echo "$target_contact_points" | jq -r -c 'del(.[] | select(.name == "email receiver")) | .[]')
+
+    # If there are any contact points left, map their names to their UIDs
+    if [ -n "$filtered_contact_points" ]; then
+        while read -r target_contact_point;
+        do 
+            name="$(echo "$target_contact_point" | jq -r '.name' | xargs)"
+            uid="$(echo "$target_contact_point" | jq -r '.uid' | xargs)"
+            target_contact_name_to_uid[$name]=$uid
+        done <<< "$filtered_contact_points"
     fi
-done
 
-source_notification_policies_export="$(curl -s \
-    -H 'Accept: application/json' \
-    -H "Authorization: Bearer $source_bearer" \
-    "https://$source_address/grafana/api/v1/provisioning/policies" )"
+    # For each source contact point (excluding "email receiver"), import it to the target instance
+    echo "$source_contact_points" | jq -r -c 'del(.[] | select(.name == "email receiver")) | .[]' | while read -r source_contact_point;
+    do  
+        source_contact_point_name=$(echo "$source_contact_point" | jq -r '.name' | xargs)
+        source_contact_point_export="$(echo $source_contact_point | jq -r 'del(.uid)')"
 
-curl -s \
-    --request "PUT"\
-    -d "$source_notification_policies_export"\
-    -H 'Content-Type: application/json' \
-    -H 'Accept: application/json' \
-    -H 'X-Disable-Provenance: true' \
-    -H "Authorization: Bearer $target_bearer" \
-    "https://$target_address/grafana/api/v1/provisioning/policies" 
+        # Import the source contact point to the target instance. Source contact point already exists in the target it will be added as a contact point integration.
+        curl -s \
+            -d "$source_contact_point_export" \
+            -H 'X-Disable-Provenance: true' \
+            -H 'Content-Type: application/json' \
+            -H 'Accept: application/json' \
+            -H "Authorization: Bearer $target_bearer" \
+            "https://$target_address/grafana/api/v1/provisioning/contact-points"
+        
+        # If the source contact point already exists in the target instance, delete it.(Now, the contact point integration that was added replaces the orginal contact point)
+        if [[ -v "target_contact_name_to_uid[$source_contact_point_name]" ]]; then
+            curl -s \
+                --request "DELETE" \
+                -H "Authorization: Bearer $target_bearer" \
+                "https://$target_address/grafana/api/v1/provisioning/contact-points/${target_contact_name_to_uid[$source_contact_point_name]}"
+        fi
+    done
+}
 
+#Function to copy notification policies from source instance to target instance. All notification policies in the target instance(doesn't matter the policy) will be replaced by the source instance notification policies.
+copy_notification_policies() {
+    # Define local variables for source and target Grafana instances
+    local source_bearer=$1
+    local source_address=$2
+    local target_bearer=$3
+    local target_address=$4
 
-# Get all folders from source Grafana
-source_folders="$(curl -s \
-    -H "Authorization: Bearer $source_bearer" \
-    "https://$source_address/grafana/api/folders")"
+    # Fetch notification policy tree from source Grafana instance
+    source_notification_policies_export="$(curl -s \
+        -H 'Accept: application/json' \
+        -H "Authorization: Bearer $source_bearer" \
+        "https://$source_address/grafana/api/v1/provisioning/policies" )"
 
-echo "$source_folders" | jq -r -c '.[]' | while read -r source_folder;
-do  
-    source_folder_title=$(echo "$source_folder" | jq -r '.title' | xargs)
+    # Push the fetched notification policy tree to the target Grafana instance. It replaces the notification policy tree in the target instance.
     curl -s \
-        -d '{"title": "'"$source_folder_title"'"}' \
+        --request "PUT"\
+        -d "$source_notification_policies_export"\
         -H 'Content-Type: application/json' \
         -H 'Accept: application/json' \
-        -H "Authorization: Bearer $target_bearer" \
-        "https://$target_address/grafana/api/folders"
-done
-
-# Get all the target folders
-target_folders="$(curl -s \
-    -H "Authorization: Bearer $target_bearer" \
-    "https://$target_address/grafana/api/folders")"
-
-source_alerts="$(curl -s \
-    -H "Authorization: Bearer $source_bearer" \
-    "https://$source_address/grafana/api/v1/provisioning/alert-rules")"
-
-target_alerts="$(curl -s \
-    -H "Authorization: Bearer $target_bearer" \
-    "https://$target_address/grafana/api/v1/provisioning/alert-rules")"
-
-echo "$source_alerts" | jq -r -c '.[]' | while read -r source_alert;
-do  
-    source_alert_title="$(echo "$source_alert" | jq -r '.title')"
-    source_folder_uid="$(echo "$source_alert" | jq -r '.folderUID')"
-    source_folder_title="$(echo "$source_folders" | jq -r --arg folder_uid "$source_folder_uid" '.[] | select(.uid==$folder_uid) | .title' | xargs)"
-
-    # Ensure target_folder_uid is retrieved correctly
-    target_folder_uid="$(echo "$target_folders" | jq -r --arg folder_title "$source_folder_title" '.[] | select(.title == $folder_title) | .uid' | xargs)"
-
-    # Update source_alert with the new folderUID and remove unnecessary fields
-    source_alert_export="$(echo "$source_alert" | jq -r -c 'del(.uid, .updated) | .folderUID=$new_folder_uid' --arg new_folder_uid "$target_folder_uid")"
-
-    # Fetch existing target_alert_uid if it exists
-    target_alert_uid="$(echo "$target_alerts" | jq -r --arg folder_uid "$target_folder_uid" --arg alert_title "$source_alert_title" '.[] | select(.title == $alert_title and .folderUID == $folder_uid) | .uid' | xargs)"
-
-    # Conditional deletion of the existing alert if its UID is found
-    if [ "$target_alert_uid" != "null" ] && [ "$target_alert_uid" != "" ]; then
-        curl -s \
-            --request "DELETE" \
-            -H "Authorization: Bearer $target_bearer" \
-            "https://$target_address/grafana/api/v1/provisioning/alert-rules/$target_alert_uid"
-    fi
-
-    # Import the modified alert
-    curl -s \
-        -d "$source_alert_export" \
         -H 'X-Disable-Provenance: true' \
-        -H 'Content-Type: application/json' \
-        -H 'Accept: application/json' \
         -H "Authorization: Bearer $target_bearer" \
-        "https://$target_address/grafana/api/v1/provisioning/alert-rules"
-done
+        "https://$target_address/grafana/api/v1/provisioning/policies"
+}
+
+#Function to copy folders from source instance to target instance. Adds folders that exist in the source instance but not in the target instance. Keeps folders that only exists in the target instance.
+copy_folders() {
+    # Define local variables for source and target Grafana instances
+    local source_bearer=$1
+    local source_address=$2
+    local target_bearer=$3
+    local target_address=$4
+
+    # Fetch all folders from source Grafana instance
+    source_folders="$(curl -s \
+        -H "Authorization: Bearer $source_bearer" \
+        "https://$source_address/grafana/api/folders")"
+
+    # For each folder in the source Grafana instance
+    echo "$source_folders" | jq -r -c '.[]' | while read -r source_folder;
+    do  
+        # Extract the folder title
+        source_folder_title=$(echo "$source_folder" | jq -r '.title' | xargs)
+        
+        # Create a new folder with the same title in the target Grafana instance. If a folder with the same title already exists nothing will happen.
+        curl -s \
+            -d '{"title": "'"$source_folder_title"'"}' \
+            -H 'Content-Type: application/json' \
+            -H 'Accept: application/json' \
+            -H "Authorization: Bearer $target_bearer" \
+            "https://$target_address/grafana/api/folders"
+    done
+}
+
+#Function to copy alerts from source instance to target instance. Adds alerts that exist in the source instance but not in the target instance. Keeps alerts that only exists in the target instance. If an alert with the same title and folder UID already exists in the target instance, it will be replaced by the source instance alert.
+copy_alerts() {
+    # Define local variables for source and target Grafana instances
+    local source_bearer=$1
+    local source_address=$2
+    local target_bearer=$3
+    local target_address=$4
+
+    # Fetch all folders from target Grafana instance
+    target_folders="$(curl -s \
+        -H "Authorization: Bearer $target_bearer" \
+        "https://$target_address/grafana/api/folders")"
+
+    # Fetch all alert rules from source Grafana instance
+    source_alerts="$(curl -s \
+        -H "Authorization: Bearer $source_bearer" \
+        "https://$source_address/grafana/api/v1/provisioning/alert-rules")"
+
+    # Fetch all alert rules from target Grafana instance
+    target_alerts="$(curl -s \
+        -H "Authorization: Bearer $target_bearer" \
+        "https://$target_address/grafana/api/v1/provisioning/alert-rules")"
+
+    # For each alert rule in the source Grafana instance
+    echo "$source_alerts" | jq -r -c '.[]' | while read -r source_alert;
+    do  
+        # Extract the alert title and folder UID
+        source_alert_title="$(echo "$source_alert" | jq -r '.title')"
+        source_folder_uid="$(echo "$source_alert" | jq -r '.folderUID')"
+
+        # Find the title of the folder that contains the alert
+        source_folder_title="$(echo "$source_folders" | jq -r --arg folder_uid "$source_folder_uid" '.[] | select(.uid==$folder_uid) | .title' | xargs)"
+
+        # Find the UID of the folder in the target Grafana instance that has the same title
+        target_folder_uid="$(echo "$target_folders" | jq -r --arg folder_title "$source_folder_title" '.[] | select(.title == $folder_title) | .uid' | xargs)"
+
+        # Update the folder UID in the alert rule and remove unnecessary fields
+        source_alert_export="$(echo "$source_alert" | jq -r -c 'del(.uid, .updated) | .folderUID=$new_folder_uid' --arg new_folder_uid "$target_folder_uid")"
+
+        # Check if an alert rule with the same title and folder UID already exists in the target Grafana instance
+        target_alert_uid="$(echo "$target_alerts" | jq -r --arg folder_uid "$target_folder_uid" --arg alert_title "$source_alert_title" '.[] | select(.title == $alert_title and .folderUID == $folder_uid) | .uid' | xargs)"
+
+        # If such an alert rule exists in target instance, delete it
+        if [ "$target_alert_uid" != "null" ] && [ "$target_alert_uid" != "" ]; then
+            curl -s \
+                --request "DELETE" \
+                -H "Authorization: Bearer $target_bearer" \
+                "https://$target_address/grafana/api/v1/provisioning/alert-rules/$target_alert_uid"
+        fi
+
+        # Import the modified alert rule into the target Grafana instance
+        curl -s \
+            -d "$source_alert_export" \
+            -H 'X-Disable-Provenance: true' \
+            -H 'Content-Type: application/json' \
+            -H 'Accept: application/json' \
+            -H "Authorization: Bearer $target_bearer" \
+            "https://$target_address/grafana/api/v1/provisioning/alert-rules"
+    done
+}
+
+
+copy_contact_points "$source_bearer" "$source_address" "$target_bearer" "$target_address"
+copy_notification_policies "$source_bearer" "$source_address" "$target_bearer" "$target_address"
+copy_folders "$source_bearer" "$source_address" "$target_bearer" "$target_address"
+copy_alerts "$source_bearer" "$source_address" "$target_bearer" "$target_address"
